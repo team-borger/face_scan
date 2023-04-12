48cceb0ae7bbbde89e8b0d5968da9aa367752037
Benedict Golpeo <alanbenedictgolpeo@gmail.com>
2023-04-13 03:19:07 +0800

    smegs

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+# main.py
+from utils import FPSmetric
 from engine import Engine
-from animegan import AnimeGAN
+from faceDetection import MPFaceDetection
+from faceNet.faceNet import FaceNet
 
 if __name__ == '__main__':
-    animegan = AnimeGAN("models/Hayao_64.onnx")
-    engine = Engine(webcam_id=0, show=True, custom_objects=[animegan])
+    facenet = FaceNet(
+        detector = MPFaceDetection(),
+        onnx_model_path = "models/faceNet.onnx", 
+        anchors = "faces",
+        force_cpu = True,
+    )
+    engine = Engine(webcam_id=0, show=True, custom_objects=[facenet, FPSmetric()])
+
+    # save first face crop as anchor, otherwise don't use
+    while not facenet.detect_save_faces(engine.process_webcam(return_frame=True), output_dir="faces"):
+        continue
+
     engine.run()
